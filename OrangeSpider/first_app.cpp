a467e8031651d2c08956de178944b8647adf7014
Flo Maldonado <flo@Aureas-MacBook-Pro.local>
2025-08-12 16:57:05 -0700

    Tutorial 21: Intro to PointLights - Vulkan Game Engine Tutorial 21

--- OrangeSpider/first_app.cpp
+++ OrangeSpider/first_app.cpp
@@ -19,7 +19,9 @@ namespace lve
     struct GlobalUbo
     {
         glm::mat4 projectionView{1.f};
-        glm::vec3 lightDirection = glm::normalize(glm::vec3{1.f, -3.f, -1.f});
+        glm::vec4 ambientLightColor{1.f, 1.f, 1.f, 0.02};
+        glm::vec3 lightPosition{-1.f};
+        alignas(16)glm::vec4 lightColor{1.f}; // w is light intensity
     };
 
     FirstApp::FirstApp()
@@ -67,6 +69,7 @@ namespace lve
         
         camera.setViewTarget(glm::vec3(-1.f, -2.f, 2.f), glm::vec3(0.0f, 0.0f, 2.5f));
         auto viewerObject = LveGameObject::createGameObject();
+        viewerObject.transform.translation.z = -2.5f;
         KeyboardMovementController cameraController{};
         
         auto currentTime = std::chrono::high_resolution_clock::now();
@@ -85,7 +88,7 @@ namespace lve
             camera.setViewYXZ(viewerObject.transform.translation, viewerObject.transform.rotation);    
             
             float aspect = lveRenderer.getAspectRatio();
-            camera.setPerspectiveProjection(glm::radians(50.f), aspect, 0.1f, 10.f);
+            camera.setPerspectiveProjection(glm::radians(50.f), aspect, 0.1f, 500.f);
             
             if (auto commandBuffer = lveRenderer.beginFrame())
             {
@@ -120,16 +123,23 @@ namespace lve
         std::shared_ptr<LveModel> lveModel = LveModel::createModelFromFile(lveDevice, "/Users/flo/LocalDocuments/Projects/VulkanLearning/GalaTutorial/GalaTutorial/models/flat_vase.obj");
         auto flatVase = LveGameObject::createGameObject();
         flatVase.model = lveModel;
-        flatVase.transform.translation = {-.5f, .5f, 2.5f};
+        flatVase.transform.translation = {-.5f, .5f, 0.0f};
         flatVase.transform.scale = {3.f, 1.5f, 3.f};
         gameObjects.push_back(std::move(flatVase));
 
         lveModel = LveModel::createModelFromFile(lveDevice, "/Users/flo/LocalDocuments/Projects/VulkanLearning/GalaTutorial/GalaTutorial/models/smooth_vase.obj");
         auto smoothVase = LveGameObject::createGameObject();
         smoothVase.model = lveModel;
-        smoothVase.transform.translation = {.5f, .5f, 2.5f};
+        smoothVase.transform.translation = {.5f, .5f, 0.0f};
         smoothVase.transform.scale = {3.f, 1.5f, 3.f};
         gameObjects.push_back(std::move(smoothVase));
+        
+        lveModel = LveModel::createModelFromFile(lveDevice, "/Users/flo/LocalDocuments/Projects/VulkanLearning/GalaTutorial/GalaTutorial/models/quad.obj");
+        auto floor = LveGameObject::createGameObject();
+        floor.model = lveModel;
+        floor.transform.translation = {0.f, .5f, 0.0f};
+        floor.transform.scale = {3.f, 1.f, 3.f};
+        gameObjects.push_back(std::move(floor));
     }
 
 }// namespace lve
--- OrangeSpider/shaders/simple_shader.vert
+++ OrangeSpider/shaders/simple_shader.vert
@@ -9,7 +9,9 @@ layout(location=0) out vec3 fragColor;
 
 layout(set=0, binding=0) uniform GlobalUbo {
     mat4 projectionViewMatrix;
-    vec3 directionToLight;
+    vec4 ambientLightColor; // w is intensity
+    vec3 lightPosition;
+    vec4 lightColor;
 } ubo;
 
 layout(push_constant) uniform Push
@@ -17,20 +19,22 @@ layout(push_constant) uniform Push
     mat4 modelMatrix;
     mat4 normalMatrix;
 } push;
-
-const float AMBIENT = 0.02;
  
 void main()
 {
-    //gl_Position = vec4(push.transform * position + push.offset, 0.0, 1.0);
-    gl_Position = ubo.projectionViewMatrix * push.modelMatrix * vec4(position, 1.0);
+
+    vec4 positionWorld = push.modelMatrix * vec4(position, 1.0);
     
-   // mat3 normalMatrix = transpose(inverse(mat3(push.modelMatrix)));
-   //vec3 normalWorldSpace = normalize(normalMatrix* normal);
+    gl_Position = ubo.projectionViewMatrix * positionWorld;
+    
+    vec3 normalWorldSpace = normalize(mat3(push.normalMatrix) * normal);
    
-   vec3 normalWorldSpace = normalize(mat3(push.normalMatrix) * normal);
+    vec3 directionToLight = ubo.lightPosition - positionWorld.xyz;
+    float attenuation = 1.0 / dot(directionToLight, directionToLight);
     
-    float lightIntensity = AMBIENT + max(dot(normalWorldSpace, ubo.directionToLight), 0);
+    vec3 lightColor = ubo.lightColor.xyz * ubo.lightColor.w * attenuation;
+    vec3 ambientLight = ubo.ambientLightColor.xyz * ubo.ambientLightColor.w;
+    vec3 diffuseLight = lightColor * max(dot(normalWorldSpace, normalize(directionToLight)), 0);
     
-    fragColor = lightIntensity * color;
+    fragColor = (diffuseLight + ambientLight) * color;
 }
